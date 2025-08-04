ba3cdc3bbd186974a7a361a69024b433152b9cf3
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-08-04 15:53:03 +0200

    py-lockdown-cafe solution

---
+++ app/cafe.py
@@ -0,0 +1,20 @@
+from app.errors import NotVaccinatedError, OutdatedVaccineError, NotWearingMaskError
+from datetime import date
+
+
+class Cafe:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor:
+            raise NotVaccinatedError("Visitor must be vaccinated")
+        elif visitor["vaccine"]["expiration_date"] < date.today():
+            raise OutdatedVaccineError("The vaccine is expired")
+
+        if "wearing_a_mask" not in visitor:
+            raise NotWearingMaskError("Visitor must wear mask")
+        else:
+            if not visitor["wearing_a_mask"]:
+                raise NotWearingMaskError("Visitor must wear mask")
+        return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+from app.errors import VaccineError, NotWearingMaskError
+from app.cafe import Cafe
+
+
+def go_to_cafe(cafe: Cafe, friends: list) -> str:
+    masks_to_buy = 0
+    vaccine_error = 0
+    for friend in friends:
+        try:
+            cafe.visit_cafe(friend)
+        except NotWearingMaskError as e:
+            masks_to_buy += 1
+        except VaccineError as e:
+            vaccine_error += 1
+    if vaccine_error > 0:
+        return f"All friends should be vaccinated"
+    if masks_to_buy > 0:
+        return f"Friends should buy {masks_to_buy} masks"
+    return f"Friends can go to {cafe.name}"
